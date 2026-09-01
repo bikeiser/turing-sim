f4e8c0e2996dcce74d13eb5b62faf82ea7baf658
Brage Isak Keiserås <bikeiser@ifi.uio.no>
2026-09-01 17:19:29 +0200

    add main file

--- exe/Main.hs
+++ exe/Main.hs
@@ -1,4 +1,41 @@
 module Main where
 
+import System.Environment (getArgs)
+import System.Exit (exitFailure)
+
+import Parser
+import Turing
+import Data.List (intercalate)
+
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = do
+    args <- getArgs
+
+    case args of 
+        ["-f", path, input] -> do
+            tmDesc <- readFile path
+            runTuring tmDesc input
+
+        [input] -> do
+            tmDesc <- getContents
+            runTuring tmDesc input
+
+        _ -> do
+            putStrLn "Usage:"
+            putStrLn "  program -f <file> <input>"
+            putStrLn "  program <input>"
+            exitFailure
+    
+runTuring :: String -> String -> IO ()
+runTuring machineText input =
+    case parseString machineText of
+        Left err -> do
+            print err
+            exitFailure
+        Right tm -> do
+            if not (wellFormed tm) then do
+                putStrLn "Invalid Turing Machine description"
+                exitFailure
+            else
+                putStrLn $ intercalate "\n" (map show (turingTrace tm input))
+
